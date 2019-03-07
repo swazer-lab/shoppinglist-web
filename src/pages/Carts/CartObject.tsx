e193086243ad6aff394d7a601ac2fd161269eb0f
Yaman KATBY <m.yaman.katby@gmail.com>
2019-03-07 12:25:10 +0300

    - Work on CartObject design.

--- src/pages/Carts/CartObject.tsx
+++ src/pages/Carts/CartObject.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Cart } from '../../types/api';
+import { Cart, CartItem } from '../../types/api';
 
 import './styles.scss';
 
@@ -9,14 +9,29 @@ interface Props {
 
 const CartObject = (props: Props) => {
 	const { title, notes, items } = props.cart;
+
+	const renderItems = (status: string) => items.filter(item => item.status === status).map(item => (
+		<div className='cart_object__items__item'>
+			<i
+				className='material-icons create_cart__cart_item__close_button'
+				children={item.status === 'active' ? 'check_box_outline_blank' : 'check_box'}
+			/>
+			<span className='cart_object__items__item__title'>{item.title}</span>
+		</div>
+	));
+
 	return (
 		<div className='cart_object'>
-			<h4>{title}</h4>
-			<h6>{notes}</h6>
+			<h4 className='cart_object__title'>{title}</h4>
 
-			<ul>
-				{items.map(item => <li key={item.uuid}>{item.title}</li>)}
-			</ul>
+			<div className='cart_object__items'>
+				{renderItems('active')}
+				{
+					items.filter(item => item.status == 'completed').length !== 0 &&
+					<div className='cart_object__items__separator' />
+				}
+				{renderItems('completed')}
+			</div>
 		</div>
 	);
 };
