3d6d8c30cb819ec927e71813add5bd5429f24a24
Furkan Armağan VELA <armagan_bursa@hotmail.com>
2019-04-11 13:43:19 +0300

    - Fix store

--- src/config/store.ts
+++ src/config/store.ts
@@ -4,36 +4,23 @@ import createSagaMiddleware from 'redux-saga';
 
 import reducers from '../reducers';
 import rootSaga from '../sagas';
-import { updateDefaultHeaders } from '../api';
+import { createStorage } from 'indexa';
+import { State } from '../types/storage';
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewareList: any = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') middlewareList.push(createLogger());
 
-const write = (storage: any) => {
-	localStorage.setItem('root', JSON.stringify(storage));
-};
-const read = (): any => {
-	let storage = {
-		isLoggedIn: false,
-		accessToken: '',
-		isEmailConfirmed: false,
-		activeLanguage: 'en',
-	};
-	const data = localStorage.getItem('root');
+const storage = createStorage<{ storage: State }>('root');
 
-	if (data) {
-		storage = JSON.parse(data);
-	}
-	return storage;
-};
-
-const str = createStore(reducers, read(), compose(applyMiddleware(...middlewareList)));
+const str = createStore(reducers, storage.getStorage(), compose(applyMiddleware(...middlewareList)));
 sagaMiddleware.run(rootSaga);
 
 export const store = str;
 
 store.subscribe(() => {
-	write({ storage: store.getState().storage });
+	storage.setStorage({ storage: store.getState().storage }, true);
 });
+
+//updateDefaultHeaders(storage.getStorage().storage.accessToken);
