72cccf3a2b753b6685fd5b50329201c509d5e8be
Minan <minan@Minan.local>
2019-05-07 16:14:09 +0300

    Fix style of toast

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { Slide, toast, ToastContainer } from 'react-toastify';
+import { Icon } from '../index';
+
 import 'react-toastify/dist/ReactToastify.css';
+import './styles.scss'
 
 import { AlertType } from '../../types/service';
 
@@ -13,6 +16,23 @@ interface Props {
 
 const Alert = (props: Props) => {
 	const { visible, type, message, handleCloseToastr } = props;
+
+	useEffect(() => {
+		const options = {
+			autoClose: 2000,
+			type: getToastrType(type),
+			transition: Slide,
+			closeButton: false
+		};
+
+		if (visible) {
+			toast(Content, options);
+		}
+
+		return () => {
+			handleCloseToastr();
+		};
+	}, [visible]);
 
 	const getToastrType = (type: AlertType) => {
 		switch (type) {
@@ -27,21 +47,29 @@ const Alert = (props: Props) => {
 		}
 	};
 
-	useEffect(() => {
-		const options = {
-			autoClose: 2000,
-			type: getToastrType(type),
-			transition: Slide,
-		};
-
-		if (visible) {
-			toast(message, options);
+	const getToastTypeToIcon = (type: AlertType) => {
+		switch ( type ) {
+			case 'warn':
+				return 'warning';
+			case 'success':
+				return 'check';
+			default: return type
 		}
+	}
 
-		return () => {
-			handleCloseToastr();
-		};
-	}, [visible]);
+	const Content = (handler: any) =>  {
+		const iconName = getToastTypeToIcon(type)
+
+		return <div className="__alert-container">
+			<Icon name={iconName} />
+
+			<span className="__message">
+				{ message }
+			</span>
+
+			<Icon name={'close'} onClick={handler.closeToast} />
+		</div>
+	}
 
 	return (
 		<ToastContainer />
