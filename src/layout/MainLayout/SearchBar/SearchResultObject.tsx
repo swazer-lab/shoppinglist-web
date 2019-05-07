641b22ce0bc3913e8c931b37113a6da6aa7a714f
Minan <minan@Minan.local>
2019-05-07 16:17:41 +0300

    Searchbar item click - open updateCart

--- src/layout/MainLayout/SearchBar/SearchResultObject.tsx
+++ src/layout/MainLayout/SearchBar/SearchResultObject.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 
 import { AppState } from '../../../types/store';
 import { Cart } from '../../../types/api';
 
+import {
+	setDraftCart,
+	setIsCartUpdating
+} from '../../../actions/carts';
+
 import './styles.scss';
 
 interface Props {
+	dispatch: Function,
+
 	cart: Cart,
+	currentUserEmail?: string
 }
 
 const SearchResultObject = (props: Props) => {
-	const { cart } = props;
+	const [accessLevel, setAccessLevel] = useState('');
+
+	const { dispatch, cart, currentUserEmail } = props;
+
+	useEffect(() => {
+		if (!currentUserEmail) return;
+
+		const currentUser = cart.users.filter(user => user.email === currentUserEmail)[0];
+
+		if (currentUser) {
+			setAccessLevel(currentUser.accessLevel);
+		}
+	}, [currentUserEmail]);
+
+
+
+	const onOpenUpdateCartModalClicked = () => {
+
+		if (accessLevel !== 'read') {
+			dispatch(setDraftCart(cart));
+			dispatch(setIsCartUpdating(true));
+		}
+	};
+
 
 	const renderItems = (status: string) => cart.items.filter(item => item.status === status).map(item => (
 		<div key={item.uuid} className='search_result_object__items__item'>
@@ -24,7 +55,7 @@ const SearchResultObject = (props: Props) => {
 	));
 
 	return (
-		<div className='search_result_object'>
+		<div className='search_result_object'  onClick={onOpenUpdateCartModalClicked}>
 			<h4 className='search_result_object__title'>{cart.title}</h4>
 
 			<div className='search_result_object__items'>
